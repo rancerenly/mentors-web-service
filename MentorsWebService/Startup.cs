bddb5da34b901dac872679d3a16bf52b9acad7a6
rancerenly <rancerenly@gmail.com>
2022-07-05 21:57:17 +0700

    added DbContextMentors.cs, change Startup.cs

--- MentorsWebService/Startup.cs
+++ MentorsWebService/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MentorsWebService.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,11 +17,16 @@ namespace MentorsWebService
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+        public Startup(IConfiguration config)
+        {
+            _configuration = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false);
             
-            services.AddMvc();
+            services.AddDbContext<DbContextMentors>(options => options.UseSqlServer(_configuration["MentorsSchool: ConnectionStrings"]));
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)

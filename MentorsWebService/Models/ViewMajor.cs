cc398244885d225dc2c74ae5387fee1374132084
rancerenly <rancerenly@gmail.com>
2022-08-16 00:55:31 +0700

    added base ViewModule for Major

---
+++ MentorsWebService/Models/ViewMajor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MentorsWebService.Models
+{
+    public class ViewMajor
+    {
+        [Required]
+        public string Title { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+    }
+}

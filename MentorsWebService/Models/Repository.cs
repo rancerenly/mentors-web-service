81d52bb4ab8b15a97d8f66a89292db2fdf6f301a
rancerenly <rancerenly@gmail.com>
2022-08-16 00:54:59 +0700

    added implementation in Repository from IRepository

--- MentorsWebService/Models/Repository.cs
+++ MentorsWebService/Models/Repository.cs
@@ -22,16 +22,41 @@ namespace MentorsWebService.Models
 
         public Teacher GetTeacher(string id) => _dbContext.Teachers.FirstOrDefault(o => o.Id == id);
         public Major GetMajor(int id) => _dbContext.Majors.FirstOrDefault(m => m.Id == id);
-        
-        public void AddTeacher(Teacher teacher)
+        public Module GetModule(int id) => _dbContext.Modules.FirstOrDefault(m => m.Id == id);
+
+        public Exercise GetExercise(int id) => _dbContext.Exercises.FirstOrDefault(m => m.Id == id);
+
+        /*public void AddTeacher(Teacher teacher)
         {
             var isExist = _dbContext.Teachers.Any(t => t.Id == null);
-            
+
             if (!isExist)
             {
                 _dbContext.Teachers.Add(teacher);
             }
-            
+
+            _dbContext.SaveChanges();
+        }*/
+
+        public void AddModule(Module module)
+        {
+            _dbContext.Modules.Add(module);
+            _dbContext.SaveChanges();
+        }
+
+        public void AddExercise(Exercise exercise)
+        {
+            _dbContext.Exercises.Add(exercise);
+            _dbContext.SaveChanges();
+        }
+
+        public void AddMajor(Major major)
+        {
+            Major newMajor = _dbContext.Majors.FirstOrDefault(o => o.Id == major.Id);
+            if (newMajor == null)
+            {
+                _dbContext.Majors.Add(major);
+            }
             _dbContext.SaveChanges();
         }
     }
